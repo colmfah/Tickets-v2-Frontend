1af27d37687c32779baa16f16e78714b7107f72e
Colm <colmdfahey@gmail.com>
2020-08-07 23:22:55 +0100

    grid on eventdetails page

--- src/Components/CreateEventComponents/EventDetails.js
+++ src/Components/CreateEventComponents/EventDetails.js
@@ -11,16 +11,113 @@ import '../../Styles/Nav.css'
 
 export class EventDetails extends Component {
 
+    constructor(){
+        super()
+        this.changeStartDetails= this.changeStartDetails.bind(this)
+        }
+
     state= {
         errors: [],
         borderColors:{
             title: 'none',
             description: 'none',
             region: 'none',
-            startDetails: 'none',
-            endDetails: 'none',
+            startDetails: '#e2e2e2',
+            endDetails: '#e2e2e2',
             eventPassword: 'none',  
+        },
+        region: '',
+        title: '',
+        description: '',
+        startDetails: '',
+        endDetails: '',
+        eventPassword: '',
+
+
+
+        titleErrors: [],
+        regionErrors: [],
+        startDetailsErrors: [],
+    }
+    componentDidMount(){
+        //initialise state to the values from props (in case user 'goes back')
+    }
+
+    changeField = (e, field) => {
+        
+        e.preventDefault()
+        let stateCopy = this.state
+        let value 
+        if (field === "startDetails" || field === "endDetails") {value = e}
+        else{value = e.target.value }
+        stateCopy[field] = value
+        if(field === 'description' || e.target.value !== ''){
+            stateCopy.borderColors[field] = '#00988f'
+        }else{
+            stateCopy.borderColors[field] = 'tomato'  
         }
+        this.setState(stateCopy)
+      
+    }
+
+    changeRegion = (e) => {
+        e.preventDefault()
+        let region = e.target.value
+        let borderColors = this.state.borderColors
+        let regionErrors = []
+        if(e.target.value === ''){
+            regionErrors.push('Please Select A Region')
+            borderColors.title = 'tomato'  
+        }else{
+            borderColors.region = '#00988f'  
+        }
+        this.setState({region, borderColors, regionErrors})
+    }
+
+    changeTitle = (e) => {
+        e.preventDefault()
+        let title = e.target.value
+        let borderColors = this.state.borderColors      
+        let titleErrors = []
+        if(e.target.value === ''){
+            titleErrors.push('Please Name Your Event')
+            borderColors.title = 'tomato'  
+        }else{
+            borderColors.title = '#00988f'  
+        }
+        this.setState({title, borderColors, titleErrors})
+    }
+
+    changeDescription = (e) => {
+        e.preventDefault()
+        let description = e.target.value
+        let borderColors = this.state.borderColors
+        borderColors.description = '#00988f' 
+        this.setState({borderColors, description})
+    }
+
+    changeStartDetails = (e) => {
+    
+        e.preventDefault()
+        let borderColors = this.state.borderColors
+        let startDetails = this.state.startDetails
+        let startDetailsErrors = []
+
+        console.log('moment(e)', moment(e).format('DD MM YY HH:mm'))
+        console.log('moment()', moment().format('DD MM YY HH:mm'));
+        console.log('!moment(e).isAfter(moment())', !moment(e).isAfter(moment()));
+        
+        
+        
+
+        if(!moment(e).isAfter(moment())){
+            startDetailsErrors.push('Your Event Must Start In The Future')
+            borderColors.startDetails = 'tomato'
+        }else{
+            startDetails = e
+            borderColors.startDetails = '#00988f'       
+        }
+        this.setState({startDetails, borderColors, startDetailsErrors})
     }
 
     continue = (e, values) => { 
@@ -65,10 +162,16 @@ export class EventDetails extends Component {
 
     }
 
+    //  
+
     borderColor = (e, field) => {
         e.preventDefault()
+        let value 
+        if (field === "startDetails" || field === "endDetails") {value = e}
+        else{value = e.target.value }
+        
         let borderColors = this.state.borderColors
-        e.target.value === '' ? borderColors[field] = 'tomato' : borderColors[field] = '#00988f'
+        value === '' ? borderColors[field] = 'tomato' : borderColors[field] = '#00988f'
         if(field ==='description'){borderColors[field] = '#00988f'}
 
         this.setState({borderColors})
@@ -78,7 +181,11 @@ export class EventDetails extends Component {
     render() {
         const {values} = this.props
         let selectColor
-        values.region === '' ? selectColor = 'rgb(118, 118, 118)' : selectColor = 'black'
+        this.state.region === '' ? selectColor = 'rgb(118, 118, 118)' : selectColor = 'black'
+
+
+
+
         return (
             <>
                 <div className="pageGrid2Rows">
@@ -94,59 +201,60 @@ export class EventDetails extends Component {
                                     <div class ="content">
 
                                         <form>
-                                
+                                            {this.state.titleErrors.map((e,i)=><p className='warning' key={i}>{e}</p>)} 
                                             <div className="group">
                                                 <input
                                                     required
-                                                    value={values.title}
-                                                    onChange={(event) => this.props.changeField(event, 'title')}
-                                                    onBlur = {event => this.borderColor(event, 'title')}
+                                                    value={this.state.title}
+                                                    onChange={event => this.changeTitle(event)}
+                                                    onBlur = {event => this.changeTitle(event)}
                                                     type='text'
                                                     placeholder='Event Name'
                                                     style={{borderColor: this.state.borderColors.title}}
-                                                    
-                                             
-                                                    />
+                                                />
+                                                
+                                                
                                             </div>
                                     
                                             <div className="group">
                                                 <textarea
-                                                    value={values.description}
+                                                    value={this.state.description}
                                                     required
-                                                    onBlur = {event => this.borderColor(event, 'description')}
-                                                    onChange={event => this.props.changeField(event, 'description')}
+                                                    onBlur = {event => this.changeDescription(event)}
+                                                    onChange={event => this.changeDescription(event)}
                                                     type='text'
                                                     placeholder='Describe The Event'
                                                     style={{borderColor: this.state.borderColors.description}}
                                                 />
                                             </div>
-                                    
+                                            
+                                            {this.state.regionErrors.map((e,i)=><p className='warning'>{e}</p>)}
                                             <div className="group">
                                                 <select
                                                     required		
-                                                    value={values.region}
-                                                    onBlur = {event => this.borderColor(event, 'region')}
-                                                    onChange={event=>this.props.changeField(event, 'region')}                                 
+                                                    value={this.state.region}
+                                                    onBlur = {event => this.changeRegion(event)}
+                                                    onChange={event => this.changeRegion.bind(this, event)}                               
                                                     style={{ color: selectColor, borderColor: this.state.borderColors.region }}
-                                                 
                                                 >
                                                     <option value="" disabled>Select your Region</option>
                                                     <option value="dublin">Dublin</option>
                                                     <option value="cork">Cork</option>
                                                     <option value="other">Other</option>
                                                 </select>
+                                                
                                             </div>
                                     
 
                                     
-                                    
-                                            <div className="group">
+                                            {this.state.startDetailsErrors.map((e,i)=><p className='warning' key={i}>{e}</p>)}
+                                            <div className="group datePickerDiv" style={{borderColor: this.state.borderColors.startDetails }}>
                                                 <DatePicker
                                                     className="datePicker"
                                                     timeIntervals={15}
-                                                    selected={values.startDetails}
-                                                    onBlur = {event => this.borderColor(event, 'startDetails')}
-                                                    onChange={(event) => this.props.changeField(event, "startDetails")}
+                                                    selected={this.state.startDetails}
+                                                    onBlur = {(event)=> this.changeStartDetails(event)}
+                                                    onChange={(event)=> this.changeStartDetails(event)}
                                                     showTimeSelect
                                                     dateFormat="d MMM yyyy HH:mm"
                                                     required
@@ -154,30 +262,30 @@ export class EventDetails extends Component {
                                                     style={{borderColor: this.state.borderColors.startDetails}}
                                                 />
                                             </div>
+                                            
                                     
                                         
                                     
-                                            <div className="group datepicker"   style={{border: 'solid 1px green', borderRadius: '6px'}}>
+                                            <div className="group datePickerDiv" style={{borderColor: this.state.borderColors.endDetails }}>
                                                 <DatePicker
+                                                    className="datePicker"
                                                     timeIntervals={15}
-                                                    selected={values.endDetails}
+                                                    selected={this.state.endDetails}
                                                     onBlur = {event => this.borderColor(event, 'endDetails')}
-                                                    onChange={event => this.props.changeField(event, "endDetails")}
+                                                    onChange={event => this.changeField(event, "endDetails")}
                                                     showTimeSelect
                                                     dateFormat="d MMM yyyy HH:mm"
                                                     required
                                                     placeholderText={'Date & Time Event Ends'}
-                                                    style={{borderColor: this.state.borderColors.endDetails}}
-                                                    
                                                 />
                                             </div>
                                     
                                             <div className="group">
                                                 <input
-                                                    value={values.eventPassword}
+                                                    value={this.state.eventPassword}
                                                     required
                                                     onBlur = {event => this.borderColor(event, 'eventPassword')}
-                                                    onChange={event => this.props.changeField(event, 'eventPassword')}
+                                                    onChange={event => this.changeField(event, 'eventPassword')}
                                                     type='password'
                                                     placeholder='Password To Check Customers In'
                                                     style={{borderColor: this.state.borderColors.eventPassword}}
